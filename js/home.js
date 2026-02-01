74162dc5a61e8e01529313a56ab509893bcc39f7
codepageme <codepage.me@gmail.com>
2026-02-01 10:58:14 +0100

    cloud home gsaped

--- js/home.js
+++ js/home.js
@@ -6,6 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((res) => res.text())
     .then((html) => {
       mount.innerHTML = html;
+
+      // Run animation AFTER component is injected
+      if (window.gsap) {
+        gsap.to(".big-floating-cloud", {
+          y: 30,
+          repeat: -1,
+          yoyo: true,
+          duration: 4.5,
+          ease: "sine.inOut"
+        });
+      }
     })
     .catch((err) => console.error("Home load failed:", err));
 });
